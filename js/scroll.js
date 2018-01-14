c65affeaeb27ecaca032ea398df9b68f7ce9c816
Ilya Suglobov <il421@mail.ru>
2018-01-14 20:47:54 +1300

    14/01_08:47 Few improvements

--- js/scroll.js
+++ js/scroll.js
@@ -1,45 +1,41 @@
 'use strict';
 (function () {
-  window.scrollToTop = function (element, coordinateY) {
-    document.addEventListener('scroll', function() {
-      if (window.pageYOffset > 0) {
-        element.classList.remove('invisible');
-      } else {
-        element.classList.add('invisible');
-      }
-    });
+  window.scrolling = {
+    showButtonToScrollUp: function (callback) {
+      document.addEventListener('scroll', callback)
+    },
 
-    element.addEventListener('click', function () {
-      window.scrollTo(0, coordinateY);
-    });
-  };
+    scrollToTop: function (element, callback) {
+      element.addEventListener('click', callback)
+    },
 
-  window.scrollToElement = function (anchors) {
-    var animationTime = 300;
-    var framesCount = 25;
+    scrollToElement: function (anchors) {
+      var animationTime = 300;
+      var framesCount = 25;
 
-    anchors.forEach(function(item) {
-      item.addEventListener('click', function(evt) {
-        // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-        var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
-        evt.preventDefault();
+      anchors.forEach(function(item) {
+        item.addEventListener('click', function(evt) {
+          // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+          var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+          evt.preventDefault();
 
-        var scroller = setInterval(function() {
-          // считаем на сколько скроллить за 1 такт
-          var scrollBy = coordY / framesCount;
+          var scroller = setInterval(function() {
+            // считаем на сколько скроллить за 1 такт
+            var scrollBy = coordY / framesCount;
 
-          // если к-во пикселей для скролла за 1 такт больше расстояния до элемент и дно страницы не достигнуто
-          if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-            // то скроллим на к-во пикселей, которое соответствует одному такту
-            window.scrollBy(0, scrollBy);
-          } else {
-            // иначе добираемся до элемента и выходим из интервала
-            window.scrollTo(0, coordY);
-            clearInterval(scroller);
-          }
-        // время интервала равняется частному от времени анимации и к-ва кадров
-        }, animationTime / framesCount);
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемент и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+              // то скроллим на к-во пикселей, которое соответствует одному такту
+              window.scrollBy(0, scrollBy);
+            } else {
+              // иначе добираемся до элемента и выходим из интервала
+              window.scrollTo(0, coordY);
+              clearInterval(scroller);
+            }
+          // время интервала равняется частному от времени анимации и к-ва кадров
+          }, animationTime / framesCount);
+        });
       });
-    });
-  };
+    }
+  }
 })();
--- js/style.js
+++ js/style.js
@@ -4,14 +4,29 @@
   var btnToBack = document.querySelector('.btn-back');
   var pageHeader = document.querySelector('.page-header');
   var anchorsOfMenu = document.querySelectorAll('a[href*="#"]');
-  var topCoordinate = window.utiles.gettingCoordinats(pageHeader);
 
+
+  var scrollUp = function () {
+    var topCoordinate = window.utiles.gettingCoordinats(pageHeader);
+    window.scrollTo(0, topCoordinate);
+  };
+
+  var hideAndShowBtn = function () {
+    if (window.pageYOffset > 0) {
+      btnToBack.classList.remove('invisible');
+    } else {
+      btnToBack.classList.add('invisible');
+    }
+  };
 
   //SLIDER
   setInterval(window.nextSlide, 2000);
 
   //SCROLL TO TOP
-  window.scrollToTop(btnToBack, topCoordinate);
+  window.scrolling.scrollToTop(btnToBack, scrollUp);
+  //SCROLL TO SHOW
+  window.scrolling.showButtonToScrollUp(hideAndShowBtn);
   //SCROLL TO ELEMENT
-  window.scrollToElement(anchorsOfMenu);
+  window.scrolling.scrollToElement(anchorsOfMenu);
+
 })()
