a627a9a686ad311a0f7ead05c2a7484e2b504d85
Ilya Suglobov <il421@mail.ru>
2018-01-14 16:07:06 +1300

    14/01_4:06 Few corrections

--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,46 @@
 'use strict';
 (function () {
   window.scrollToTop = function (element, coordinateY) {
+    document.addEventListener('scroll', function() {
+      if (window.pageYOffset > 0) {
+        element.classList.remove('invisible');
+      } else {
+        element.classList.add('invisible');
+      }
+    });
+
     element.addEventListener('click', function () {
       window.scrollTo(0, coordinateY);
     });
   };
 
+  window.scrollToElement = function (anchors) {
+    var animationTime = 300;
+    var framesCount = 25;
 
+    anchors.forEach(function(item) {
+      item.addEventListener('click', function(evt) {
+        var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top; // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+        evt.preventDefault();
+        console.log(coordY);
 
-  // menuLinks.addEventListener('click', function() {
-  //   var coordinateY = gettingCoordinats(sectionShop);
-  //
-  //   var start = Date.now(); // сохранить время начала
-  //
-  //   var timer = setInterval(function() {
-  //     // вычислить сколько времени прошло из opts.duration
-  //     var timePassed = (Date.now() - start) / 3;
-  //     console.log(timePassed);
-  //
-  //     window.scrollTo(0, timePassed);
-  //
-  //     if (timePassed > coordinateY.top) clearInterval(timer);
-  //
-  //   }, 50);
-  //
-  //
-  // });
+        var scroller = setInterval(function() {
+          // считаем на сколько скроллить за 1 такт
+          var scrollBy = coordY / framesCount;
+
+          // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+          // и дно страницы не достигнуто
+          if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+            // то скроллим на к-во пикселей, которое соответствует одному такту
+            window.scrollBy(0, scrollBy);
+          } else {
+            // иначе добираемся до элемента и выходим из интервала
+            window.scrollTo(0, coordY);
+            clearInterval(scroller);
+          }
+        // время интервала равняется частному от времени анимации и к-ва кадров
+        }, animationTime / framesCount);
+      });
+    });
+  };
 })();
--- js/style.js
+++ js/style.js
@@ -1,19 +1,17 @@
 'use strict';
 
-var btnToBack = document.querySelector('.btn-back');
-var pageHeader = document.querySelector('.page-header');
-var topCoordinate = window.utiles.gettingCoordinats(pageHeader);
+(function () {
+  var btnToBack = document.querySelector('.btn-back');
+  var pageHeader = document.querySelector('.page-header');
+  var anchorsOfMenu = document.querySelectorAll('a[href*="#"]');
+  var topCoordinate = window.utiles.gettingCoordinats(pageHeader);
 
-//SLIDER
-setInterval(window.nextSlide, 2000);
 
-//SCROLL TO TOP
-document.addEventListener('scroll', function() {
-  if (window.pageYOffset > 0) {
-    btnToBack.classList.remove('invisible');
-  } else {
-    btnToBack.classList.add('invisible');
-  }
-});
+  //SLIDER
+  setInterval(window.nextSlide, 2000);
 
-window.scrollToTop(btnToBack, topCoordinate);
+  //SCROLL TO TOP
+  window.scrollToTop(btnToBack, topCoordinate);
+  //SCROLL TO ELEMENT
+  window.scrollToElement(anchorsOfMenu);
+})()
