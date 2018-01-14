62986b73864fc384109d6d34f4d9c47ce49b45d0
Ilya Suglobov <il421@mail.ru>
2018-01-14 22:03:13 +1300

    14/01_10:02 Few improvements

--- js/scroll.js
+++ js/scroll.js
@@ -9,32 +9,9 @@
       element.addEventListener('click', callback)
     },
 
-    scrollToElement: function (anchors) {
-      var animationTime = 300;
-      var framesCount = 25;
-
+    scrollToElement: function (anchors, callback) {
       anchors.forEach(function(item) {
-        item.addEventListener('click', function(evt) {
-          // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-          var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
-          evt.preventDefault();
-
-          var scroller = setInterval(function() {
-            // считаем на сколько скроллить за 1 такт
-            var scrollBy = coordY / framesCount;
-
-            // если к-во пикселей для скролла за 1 такт больше расстояния до элемент и дно страницы не достигнуто
-            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-              // то скроллим на к-во пикселей, которое соответствует одному такту
-              window.scrollBy(0, scrollBy);
-            } else {
-              // иначе добираемся до элемента и выходим из интервала
-              window.scrollTo(0, coordY);
-              clearInterval(scroller);
-            }
-          // время интервала равняется частному от времени анимации и к-ва кадров
-          }, animationTime / framesCount);
-        });
+        item.addEventListener('click', callback);
       });
     }
   }
--- js/style.js
+++ js/style.js
@@ -6,12 +6,7 @@
   var anchorsOfMenu = document.querySelectorAll('a[href*="#"]');
 
 
-  var scrollUp = function () {
-    var topCoordinate = pageHeader.getBoundingClientRect().top;
-    window.scrollTo(0, topCoordinate);
-  };
-
-  var hideAndShowBtn = function () {
+  var hideAndShowScrollBtn = function () {
     if (window.pageYOffset > 0) {
       btnToBack.classList.remove('invisible');
     } else {
@@ -19,14 +14,43 @@
     }
   };
 
+  var scrollToMenu = function (evt) {
+    var animationTime = 300;
+    var framesCount = 25;
+    var coordY = document.querySelector(evt.target.hash).getBoundingClientRect().top;
+
+    evt.preventDefault();
+
+    var scroller = setInterval(function() {
+      // считаем на сколько скроллить за 1 такт
+      var scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемент и дно страницы не достигнуто
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  };
+
+  var scrollUp = function () {
+    var topCoordinate = pageHeader.getBoundingClientRect().top;
+    window.scrollTo(0, topCoordinate);
+  };
+
   //SLIDER
   setInterval(window.nextSlide, 2000);
 
+  //SCROLL TO SHOW
+  window.scrolling.showButtonToScrollUp(hideAndShowScrollBtn);
+  //SCROLL TO ELEMENT
+  window.scrolling.scrollToElement(anchorsOfMenu, scrollToMenu);
   //SCROLL TO TOP
   window.scrolling.scrollToTop(btnToBack, scrollUp);
-  //SCROLL TO SHOW
-  window.scrolling.showButtonToScrollUp(hideAndShowBtn);
-  //SCROLL TO ELEMENT
-  window.scrolling.scrollToElement(anchorsOfMenu);
 
 })()
